5f963ee711baec8fa497dc09122f1f9421e62d04
haxislancelot <84822175+haxislancelot@users.noreply.github.com>
2021-06-27 00:09:36 -0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,22 +2,22 @@
 # Gorillaz Injector created by haxislancelot @ GitHub, If you're going to use some code from my module, give me the credits, I'll be grateful.
 DEBUG=true
 
-if [[ ! -e "/sdcard/Gorillaz-Injector" ]]; then
-   rm -rf /sdcard/Gorillaz-Injector
+if [[ ! -e "/sdcard/Niko Logs" ]]; then
+   rm -rf /sdcard/Niko Logs
 fi
 
-mkdir "/sdcard/Gorillaz-Injector"
+mkdir "/sdcard/Niko Logs"
 
-log=/sdcard/Gorillaz-Injector/installation.log
+log=/sdcard/Niko Logs/installation.log
 echo "If this file is useless to you, you can delete it." >> $log
 
 SKIPUNZIP=0
-unzip -qjo "$ZIPFILE" 'common/git-banner.sh' -d "$TMPDIR" >&2
-. "$TMPDIR"/git-banner.sh
+unzip -qjo "$ZIPFILE" 'common/niko-banner.sh' -d "$TMPDIR" >&2
+. "$TMPDIR"/niko-banner.sh
 
 sleep 4
 ui_print ""
-ui_print "- Gorillaz-Injector is a full featured Magisk module to enhance your device experience,"
+ui_print "- Niko Tweaks is a full featured Magisk module to enhance your device experience,"
 ui_print "- Improve your gaming, ram and battery!"
 sleep 2
 ui_print ""
