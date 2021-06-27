7d562a320db9980ef0ca13a3b6e43c09efd7b5fb
haxislancelot <84822175+haxislancelot@users.noreply.github.com>
2021-06-27 00:27:25 -0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,1 +1,33 @@
+#!/system/bin/sh
 
+# Configure sqlite3
+sqlite_opt() {
+if [[ "$(find /system -name "sqlite3" -type f)" ]]; then
+for i in $(find /d* -iname "*.db"); do
+sqlite3 "$i" 'VACUUM;'
+resVac=$?
+if [[ $resVac == "0" ]]; then 
+   resVac="SUCCESS"
+else
+   resVac="FAILED(ERRCODE)-$resVac"
+fi
+sqlite3 "$i" 'REINDEX;'
+resIndex=$?
+if [[ $resIndex == "0" ]]; then 
+   resIndex="SUCCESS"
+else
+   resVac="FAILED(ERRCODE)-$resIndex"
+fi
+sqlite3 "$i" 'ANALYZE;'
+    resAnlz=$?
+    if [[ $resAnlz == "0" ]]; then 
+       resAnlz="SUCCESS"
+    else
+       resVac="FAILED(ERRCODE)-$resAnlz"
+    fi
+  done
+fi
+}
+
+# Run sqlite3
+sqlite_opt
